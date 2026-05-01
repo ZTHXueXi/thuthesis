892a89d5cff192c587b75f5c978485d4196a513d
Zeping Lee <zepinglee@gmail.com>
2026-05-01 19:09:09 +0800

    Add tests for bibliography in appendix

--- testfiles/config-bibtex.lua
+++ testfiles/config-bibtex.lua
@@ -1,14 +1,22 @@
-checksuppfiles = {"*.tex", "*.bib"}
+checksuppfiles = { "*.tex", "*.bib" }
 testfiledir = "testfiles/10-bibtex"
 
-includetests = {"*"}
+includetests = { "*" }
 excludetests = {}
 
 checkruns = 3
 
 function runtest_tasks(name, run)
   if run == 1 then
-    return "bibtex -terse " .. name
+    local aux_files = filelist(testdir, name .. "*.aux")
+    local commands = {}
+    for _, aux_file in ipairs(aux_files) do
+      local base_name = jobname(aux_file)
+      table.insert(commands, "bibtex -terse " .. base_name)
+    end
+    local command = table.concat(commands, "; ")
+    -- io.stderr:write(command .. "\n")
+    return command
   else
     return ""
   end
